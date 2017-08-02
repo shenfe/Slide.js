33544076e1f2fdf1f5f017c19b06227ee8c35a7b
keshen <hackersk@qq.com>
2017-08-02 11:37:55 +0800

    upd8

--- src/slide.js
+++ src/slide.js
@@ -24,53 +24,42 @@ var rand_351089689 = (function () {
             addCssRule('.slider-container-v .slider-wrap', 'height:100%;');
             addCssRule('.slider-container-v .slider-bar', 'position:absolute;left:0;bottom:0;width:100%;');
             addCssRule('.slider-bar', 'position:relative;');
-            addCssRule('.slider-dot', 'position:absolute;right:0;display:inline-block;transform:translate3d(0,0,0)');
-            addCssRule('.slider-container-h .slider-dot', 'transform:translate3d(50%,50%,0)');
-            addCssRule('.slider-container-v .slider-dot', 'transform:translate3d(-50%,-50%,0)');
+            addCssRule('.slider-dot', 'position:absolute;right:0;display:inline-block;-webkit-transform:translate3d(0,0,0);transform:translate3d(0,0,0);');
+            addCssRule('.slider-container-h .slider-dot', '-webkit-transform:translate3d(50%,0,0);transform:translate3d(50%,0,0);');
+            addCssRule('.slider-container-v .slider-dot', '-webkit-transform:translate3d(0,-50%,0);transform:translate3d(0,-50%,0);');
         }
-        
-        $container.innerHTML = `
-            <div class="slider-wrap">
-                <div class="slider-bar">
-                    <div class="slider-dot"></div>
-                </div>
+
+        var type = (option.dir !== 'v') ? 'h' : 'v';
+
+        var $wrap = document.createElement('div');
+        $wrap.className = 'slider-wrap';
+        $wrap.innerHTML = `
+            <div class="slider-bar">
+                <div class="slider-dot"></div>
             </div>
         `;
-        var $wrap = $container.querySelector('.slider-wrap');
-        var $bar = $container.querySelector('.slider-bar');
-        var $dot = $container.querySelector('.slider-dot');
-
-        var type = (option.dir === 'h') ? 'h' : 'v';
+        $container.appendChild($wrap);
+        var $bar = $wrap.querySelector('.slider-bar');
+        var $dot = $wrap.querySelector('.slider-dot');
         $container.classList.add('slider-container');
         $container.classList.add('slider-container-' + type);
-        
+
         var length = {
             total: type === 'h' ? $wrap.offsetWidth : $wrap.offsetHeight,
             progress: option.init || 0
         };
         $bar.style[type === 'h' ? 'width' : 'height'] = '' + length.progress + '%';
-        var offset = {
-            x: type === 'v' ? 0 : (length.progress / 100 * length.total),
-            y: type === 'h' ? 0 : (length.progress / 100 * length.total)
-        };
+        var offset = length.progress / 100 * length.total;
 
         var onchanging = option.onchanging || function () {};
         var onchanged = option.onchanged || function () {};
 
-        var setOffsetX = function (v) {
+        var setOffset = function (v) {
+            offset = v;
             if (v < 0) v = 0;
             if (v > length.total) v = length.total;
-            offset.x = v;
-            length.progress = offset.x / length.total * 100;
-            $bar.style.width = '' + length.progress + '%';
-            onchanging(length.progress);
-        };
-        var setOffsetY = function (v) {
-            if (v < 0) v = 0;
-            if (v > length.total) v = length.total;
-            offset.y = v;
-            length.progress = offset.y / length.total * 100;
-            $bar.style.height = '' + length.progress + '%';
+            length.progress = v / length.total * 100;
+            $bar.style[type === 'h' ? 'width' : 'height'] = '' + length.progress + '%';
             onchanging(length.progress);
         };
 
@@ -78,42 +67,34 @@ var rand_351089689 = (function () {
             x: null,
             y: null
         };
-        $wrap.addEventListener('touchstart', function (e) {
-            // if (e.target.classList.contains('slider-dot')) return;
+
+        var touchstartHandler = function (e) {
             var target = e.target;
             var touchobj = e.changedTouches[0];
             var distance;
             if (type === 'h') {
-                distance = touchobj.pageX - $wrap.offsetLeft;
-                setOffsetX(distance);
+                distance = touchobj.pageX - $wrap.getBoundingClientRect().left;
+                setOffset(distance);
             } else {
-                distance = $wrap.offsetTop + $wrap.offsetHeight - touchobj.pageY;
-                setOffsetY(distance);
+                distance = $wrap.getBoundingClientRect().top + $wrap.offsetHeight - touchobj.pageY;
+                setOffset(distance);
             }
-            touchPos.x = touchobj.pageX;
-            touchPos.y = touchobj.pageY;
-        }, false);
-
-        var touchstartHandler = function (e) {
-            var touchobj = e.changedTouches[0];
             touchPos.x = touchobj.pageX;
             touchPos.y = touchobj.pageY;
         };
         $wrap.addEventListener('touchstart', touchstartHandler, false);
-        // $dot.addEventListener('touchstart', touchstartHandler, false);
 
         var touchmoveHandler = function (e) {
             var touchobj = e.changedTouches[0];
             if (type === 'h') {
-                setOffsetX(offset.x - touchPos.x + touchobj.pageX);
+                setOffset(offset - touchPos.x + touchobj.pageX);
             } else {
-                setOffsetY(offset.y + touchPos.y - touchobj.pageY);
+                setOffset(offset + touchPos.y - touchobj.pageY);
             }
             touchPos.x = touchobj.pageX;
             touchPos.y = touchobj.pageY;
         };
         $wrap.addEventListener('touchmove', touchmoveHandler, false);
-        // $dot.addEventListener('touchmove', touchmoveHandler, false);
 
         var touchendHandler = function (e) {
             touchPos.x = null;
@@ -121,18 +102,13 @@ var rand_351089689 = (function () {
             onchanged(length.progress);
         };
         $wrap.addEventListener('touchend', touchendHandler, false);
-        // $dot.addEventListener('touchend', touchendHandler, false);
 
         Object.defineProperty(this, 'progress', {
             get: function () { return length.progress; },
             set: function (v) {
                 if (v < 0) v = 0;
                 if (v > 100) v = 100;
-                if (type === 'h') {
-                    setOffsetX(offset.x + length.total * v / 100);
-                } else {
-                    setOffsetY(offset.y + length.total * v / 100);
-                }
+                setOffset(offset + length.total * v / 100);
             }
         });
     };
